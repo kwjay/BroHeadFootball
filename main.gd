12cdc1303c49fbf773043f94c47913b07c30f438
KWJ <102679975+kwjay@users.noreply.github.com>
2024-10-14 12:04:47 +0200

    Add Goal Score UI

--- main.gd
+++ main.gd
@@ -3,7 +3,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$Stadium.goal_scored.connect(_on_stadium_goal_scored)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -12,4 +12,8 @@ func _process(delta):
 
 
 func _on_stadium_goal_scored(team):
-	print_debug(team + " scored a goal")
+	match team:
+		"1":
+			$UI/Score1.text = str(int($UI/Score1.text) + 1)
+		"2":
+			$UI/Score2.text = str(int($UI/Score2.text) + 1)
--- stadium.gd
+++ stadium.gd
@@ -7,6 +7,6 @@ func _ready():
 
 func _on_Goal_Scored(goalpost):
 	if goalpost == "LeftGoalpost":
-		emit_signal("goal_scored", "1")
-	elif goalpost == "RightGoalpost":
 		emit_signal("goal_scored", "2")
+	elif goalpost == "RightGoalpost":
+		emit_signal("goal_scored", "1")
