62ee12671c970023234f652867b2f1f485f984f6
KWJ <102679975+kwjay@users.noreply.github.com>
2024-10-19 23:14:47 +0200

    Clean second player controls

--- scenes/game/characters/player.gd
+++ scenes/game/characters/player.gd
@@ -3,50 +3,50 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -450.0
 const PUSH_FORCE = 100.0
-@export var WSAD = false
-
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 const FOOT_MAX_ROTATION = -2.5
 const FOOT_RESET_SPEED = 10.0
 const FOOT_KICK_SPEED = 20.0
 
-var jump = "jump"
-var kick = "kick"
-var left = "left"
-var right = "right"
-
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+@export var WSAD = false
+var player_controls
 func _ready():
 	if WSAD:
-		jump += "2"
-		kick += "2"
-		left += "2"
-		right += "2"
+		player_controls = {
+		"jump": "jump2",
+		"kick": "kick2",
+		"left": "left2",
+		"right": "right2",
+		}
+	else:
+		player_controls = {
+		"jump": "jump",
+		"kick": "kick",
+		"left": "left",
+		"right": "right",
+		}
 
 func _physics_process(delta):
-	
 	if not is_on_floor():
 		velocity.y += gravity * delta
-	if Input.is_action_just_pressed(jump) and is_on_floor():
+	if Input.is_action_just_pressed(player_controls["jump"]) and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
-	var direction = Input.get_axis(left, right)
+	var direction = Input.get_axis(player_controls["left"], player_controls["right"])
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
 	handle_foot_rotation(delta)
 	move_and_slide()
 	apply_collision_impulse()
 	
-
 func handle_foot_rotation(delta):
 	var foot = $"Foot"
-	if Input.is_action_pressed(kick) and foot.rotation > FOOT_MAX_ROTATION:
+	if Input.is_action_pressed(player_controls["kick"]) and foot.rotation > FOOT_MAX_ROTATION:
 		foot.rotation -= FOOT_KICK_SPEED * delta
-	elif foot.rotation < 0 and not Input.is_action_pressed(kick):
+	elif foot.rotation < 0 and not Input.is_action_pressed(player_controls["kick"]):
 		foot.rotation += FOOT_RESET_SPEED * delta
-		
+	
 func apply_collision_impulse():
 	for i in get_slide_collision_count():
 		var slide_collision = get_slide_collision(i)
