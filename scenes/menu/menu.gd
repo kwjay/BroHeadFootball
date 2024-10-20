73e6dcbd15b1ccddec8e55c11d98beed315ff82f
KWJ <102679975+kwjay@users.noreply.github.com>
2024-10-20 18:07:51 +0200

    Add Button Sprites

--- scenes/menu/menu.gd
+++ scenes/menu/menu.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _on_play_pressed():
-	get_tree().change_scene_to_file("res://scenes/game/game.tscn")
+	get_tree().change_scene_to_file("res://scenes/menu/character_menu.tscn")
 	self.queue_free()
 
 func _on_quit_pressed():
