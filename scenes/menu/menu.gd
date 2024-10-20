1fbc0e7f697b1108df4894e4cd4ec2c234080e34
KWJ <102679975+kwjay@users.noreply.github.com>
2024-10-20 18:30:17 +0200

    Add button action

--- scenes/menu/menu.gd
+++ scenes/menu/menu.gd
@@ -2,7 +2,6 @@ extends Control
 
 func _on_play_pressed():
 	get_tree().change_scene_to_file("res://scenes/menu/character_menu.tscn")
-	self.queue_free()
 
 func _on_quit_pressed():
 	get_tree().quit()
