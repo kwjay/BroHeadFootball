0d2f03a3d6b05a86a557dcd266188207e2cfecc1
KWJ <102679975+kwjay@users.noreply.github.com>
2024-10-20 14:51:52 +0200

    Add menu icons

--- scenes/menu/menu.gd
+++ scenes/menu/menu.gd
@@ -4,10 +4,5 @@ func _on_play_pressed():
 	get_tree().change_scene_to_file("res://scenes/game/game.tscn")
 	self.queue_free()
 
-
-func _on_help_pressed():
-	print_debug("help pressed")
-
-
 func _on_quit_pressed():
 	get_tree().quit()
